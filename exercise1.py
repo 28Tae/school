be263e0274e02c4828d8150d29098ccc0d94be30
28Tae™ <83344415+28Tae@users.noreply.github.com>
2023-02-21 19:24:00 +0800

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,26 @@
+def oval():
+    for i in range(2):
+        go(100,100,.6)
+        go(100,0,2.65)
+    
+def treePointTurn():
+    go(50,50,1)
+    go(98,0,1.35)
+    go(50,50,1)
+    go(-70,-70,2)
+    go(33,33,1)
+    go(98,0,1.35)
+    go(50,50,1.2)
+
+def infinity():
+    go(100,5,5.5)
+    go(5,100,5.5)
+
+def star():
+    for i in range(5):
+        go(80,80,2)
+        go(100,-100,1.06)
+
+"""Other than star() which is more theoretical and systematic,
+generally all these functions are not applicable,
+and serve as a geometric example."""
