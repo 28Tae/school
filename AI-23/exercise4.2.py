b60be7aa5eeee30767820c511f259e158261573d
28qian <83344415+28Tae@users.noreply.github.com>
2023-02-21 20:43:14 +0800

    Update and rename exercise 4.2.py to exercise4.2.py

--- AI-23/exercise4.2.py
+++ AI-23/exercise4.2.py
@@ -1,4 +1,4 @@
-while active():                   
+while active(): #idk why but active() only works in exp 1-7 and 1-8           
     if get_ultrasound()[0] - get_ultrasound()[1] > 30:
         go(-20,80,0.05)                                   
     elif get_ultrasound()[1] - get_ultrasound()[0] > 30:
