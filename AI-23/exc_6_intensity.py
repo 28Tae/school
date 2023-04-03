510190511f559007daf2503e653cc15aa3d7f225
28qian <83344415+28Tae@users.noreply.github.com>
2023-04-03 19:58:50 +0800

    v1 release

--- AI-23/exc_6_intensity.py
+++ AI-23/exc_6_intensity.py
@@ -3,13 +3,9 @@ def ultrasound(i):  # standard
 
 # surprisingly enough, considering exc 6.1 and 6.2 instructions,
 # intensity would be directly proportional to the power of the car
-# in other words, you can DIRECTLY get the power FROM finding intensity
-
-# obviously there's the issue of "exceeding power range"
-# where the raw power exceeds 100
-# so you MUST reduce the intensity value first (i.e. by dividing)
+# basically, you can DIRECTLY get power FROM finding intensity
 
 set(500, 60, 180)
 while ultrasound(2) > 30:
-	i = get_intensity()[0] / 2.5
-	go(i,i,0.1)
+	i = get_intensity()[0] / 2.5	# limit to acceptable range (max 100)
+	go(i, i, 0.1)
