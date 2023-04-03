b492938ac8bd85681bce1688d023ce7e9bc015e8
28qian <83344415+28Tae@users.noreply.github.com>
2023-04-03 19:53:21 +0800

    v1 release

--- AI-23/exc_4_line_wall.py
+++ AI-23/exc_4_line_wall.py
@@ -1,23 +1,13 @@
-"""
-ULTRASOUND SENSORS (relative distance to an obstacle)
-0 - ↖ frontleft 
-1 - ↗ frontright 
-2 - ↑ front 
-3 - ↓ back
-
-COLOUR SENSORS
-given get_color()[i][j],
-[i] represents the sensor 0 to 3
-[j] represents either R, G, or B
-"""
+# refer to README.org to learn about
+# ultrasound & color sensors 
 
 while active():
     l = 50
     r = 50
-    if get_ultrasound()[2] < 130: # avoids the right wall
+    if get_ultrasound()[2] < 130:   # avoid right wall
         l = -40
         r = 100
-    elif get_color()[0][0] >= 50: # avoids the red line via sensor 0
+    elif get_color()[0][0] >= 50:   # avoid red line via leftmost sensor
         l = 100
         r = -40
     go(l, r, 0.5)
