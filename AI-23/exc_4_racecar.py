114b0b8595dbd15c04689390d6dd33ad9e7d1258
28qian <83344415+28Tae@users.noreply.github.com>
2023-04-03 19:54:27 +0800

    v1 release

--- AI-23/exc_4_racecar.py
+++ AI-23/exc_4_racecar.py
@@ -1,7 +1,7 @@
-while active(): #idk why but active() only works in exp 1-7 and 1-8           
-    if get_ultrasound()[0] - get_ultrasound()[1] > 30:
-        go(-20,80,0.05)                                   
-    elif get_ultrasound()[1] - get_ultrasound()[0] > 30:
-        go(80,-20,0.05)                                    
+while active():       
+    if get_ultrasound()[0] - get_ultrasound()[1] > 30:     # relatively closer to left wall
+        go(-20, 80, 0.05)                                   
+    elif get_ultrasound()[1] - get_ultrasound()[0] > 30:   # relatively closer to right wall
+        go(80, -20, 0.05)                                    
     else:
-        go(90,90,0.05)
+        go(90, 90, 0.05)
